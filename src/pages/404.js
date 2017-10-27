90fb09a83107281ea3ddbda86f407a1e132b7b4f
paul <dongpoding@gmail.com>
2017-10-27 13:44:12 +0800

    blog version 2 power by gatsby-stater-blog from github

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function ErrorPage() {
+  // Import result is the URL of your image
+  return <font>opps there were something wrong here!</font>;
+}
+
+export default ErrorPage;
